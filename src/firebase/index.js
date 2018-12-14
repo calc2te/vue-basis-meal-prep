8744ab0f43eb7634693b3b601782b99c8a2bcace
Seobs <subi88@naver.com>
2018-12-14 15:13:37 +0900

    first commit

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyAOF3lRX-DX9qpmkGddGovwOZ7wHRbw5cs",
+  authDomain: "meal-prep-e2f3d.firebaseapp.com",
+  databaseURL: "https://meal-prep-e2f3d.firebaseio.com",
+  projectId: "meal-prep-e2f3d",
+  storageBucket: "meal-prep-e2f3d.appspot.com",
+  messagingSenderId: "823464395343"
+};
+firebase.initializeApp(config);
